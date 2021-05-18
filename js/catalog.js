e2d7ddad438a19a4d95abc2e9d628cc30f6d6922
laurazam <77395682+LauraZam@users.noreply.github.com>
2021-05-18 08:09:08 +0600

    Add files via upload

--- js/catalog.js
+++ js/catalog.js
@@ -1,3 +1,48 @@
+class room{
+    roomName;
+    roomClass;
+    roomsCount;
+    roomsPrice;
+    card_img;
+    constructor(newRoomName, newRoomClass, newRoomsCount, newRoomsPrice, newCardImg){
+        this.roomName = newRoomName
+        this.roomClass = newRoomClass
+        this.roomsCount = newRoomsCount
+        this.roomsPrice = newRoomsPrice
+        this.card_img = newCardImg
+    }
+    get roomName(){
+        return this.roomName
+    }
+    get roomClass(){
+        return this.roomClass
+    }
+    get roomsCount(){
+        return this.roomsCount
+    }
+    get roomsPrice(){
+        return this.roomsPrice
+    }
+    get card_img(){
+        return this.card_img
+    }
+    set roomName(newRoomName){
+        this.roomName = newRoomName
+    }
+    set roomClass(newRoomClass){
+        this.roomClass = newRoomClass
+    }
+    set roomsCount(newRoomsCount){
+        this.roomsCount = newRoomsCount
+    }
+    set roomsPrice(newRoomsPrice){
+        this.roomsPrice = newRoomsPrice
+    }
+    
+    set card_img(newcardImg){
+        this.card_img = newcardImg
+    }
+}
 let categories = [
     {
         name: 'price',
@@ -15,20 +60,20 @@ let categories = [
 let podcategories  =[
     [
         {
-            name: 'от 30000 kzt',
-            value: 'от 30000 kzt'
+            name: '30000 kzt',
+            value: '30000 kzt'
         },
         {
-            name: 'от 50000 kzt',
-            value: 'от 50000 kzt'
+            name: '50000 kzt',
+            value: '50000 kzt'
         },
         {
-            name: 'от 80000 kzt',
-            value: 'от 80000 kzt'
+            name: '80000 kzt',
+            value: '80000 kzt'
         },
         {
-            name: 'от 110000 kzt',
-            value: 'от 110000 kzt'
+            name: '110000 kzt',
+            value: '110000 kzt'
         }
 
     ],
@@ -69,7 +114,15 @@ let podcategories  =[
         }
     ]
 ]
+let imgs = [
+    'https://bonhotel.dp.ua/image/Standart%20Classic%20202%20203/9.jpg',
+    'https://lhotels.ru/cache/images/6/8/c/68ccb258176ca4ba105d8aca3c7d1f26.jpg',
 
+]
+let rooms = [
+    new room('Standard Room', 'Standard', 1, '30000 kzt', imgs[0]),
+    new room('Standard room', 'Standard', 1, '50000 kzt', imgs[1])
+]
 let giveCategories = () => {
     for (let i = 0; i < 1; i++) {
         $('#category1').append(`
@@ -110,30 +163,18 @@ let givPodCategories = () => {
     }
 }
 givPodCategories()
-let roomClass = $('.roomClass')
-let startFilters = $('#startFilters')
-startFilters.click(function () {
-    for (let i = 0; i < podcategories.length; i++) {
-        for (let k = 0; k < podcategories[i].length; k++) {
-            if (podcategories[i][k].value === roomClass.val()) {
-                document.getElementsByClassName('card').style.border = '1px soli red'
-            }
-            
-        }
-        
-    }
-})
+
 let drawCard = (roomName, roomClass, roomsCount, roomsPrice, card_img) => {
     $('.cards').append(`
         <div class="card">
             <div class="dCard_img">
-                <img class="card_img" src="${card_img}" alt=""></img>
+            <img src="${card_img}" alt="" class="card_img">
             </div>
             <div class="dCardText">
-                <h2 class="roomName">Standard</h2>
-                <p class="roomClass">Standard</p>
-                <p class="roomsCount">2</p>
-                <p class="roomsPrice">от 50000 kzt</p>
+                <h2 class="roomName">Name: ${roomName}</h2>
+                <p class="roomClass">Class: ${roomClass}</p>
+                <p class="roomsCount">Rooms count: ${roomsCount}</p>
+                <p class="roomsPrice">Price: ${roomsPrice}</p>
                 <input type="date" name="ot" id="">
                 <input type="date" name="do" id="">
                 <button class="startRent btn">забронировать</button>
@@ -141,4 +182,21 @@ let drawCard = (roomName, roomClass, roomsCount, roomsPrice, card_img) => {
         </div>
     `)
 }
-drawCard()
+$('#startFilters').click(function () {
+    
+    for (let i = 0; i < rooms.length; i++) {
+        for (let k = 0; k < podcategories.length; k++) {
+            if (rooms[i].roomsPrice === podcategories[0][k].value) {
+                console.log(rooms[i]);
+            }
+        }
+        
+        
+    }
+})
+
+// drawCard()
+for (let i = 0; i < rooms.length; i++) {
+    drawCard(rooms[i].roomName, rooms[i].roomClass, rooms[i].roomsCount, rooms[i].roomsPrice, rooms[i].card_img)
+    
+}
